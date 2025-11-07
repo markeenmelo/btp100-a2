36044d83abba5411aa2702a6904b0efeffd276c6
Marcos Melo <marcosmelo@proton.me>
2025-11-07 16:15:23 -0500

    boilerplate

---
+++ main.c
@@ -0,0 +1,35 @@
+#include <stdio.h>
+
+struct Employee {
+    int id;
+    char name[20];
+    char gender[20];
+    char ethnicity[20];
+    float salary;
+};
+
+void addEmployee();
+void deleteEmployee();
+void displayEmployees();
+void calculateSalary();
+void displayComparison();
+
+int main() {
+    return 0;
+}
+
+void addEmployee() {
+
+};
+void deleteEmployee() {
+
+};
+void displayEmployees() {
+
+};
+void calculateSalary() {
+
+};
+void displayComparison() {
+
+};
